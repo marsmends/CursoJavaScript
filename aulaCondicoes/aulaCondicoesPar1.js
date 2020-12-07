df21e295cc9aeb2dfefa5003c11c844514cc7c60
Marília Queiroz <marisqueiroz1@gmail.com>
2020-12-07 11:52:07 -0300

    Aula de Condições Part1
    Conceitos do if e else em JS.

---
+++ aulaCondicoes/aulaCondicoesPar1.js
@@ -0,0 +1,11 @@
+console.log("Ola mundo!")
+
+var vel = 67
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade máxima que é ${vel}.`)
+} else {
+    console.log(`Dirija com segurança!`)
+}
+
+// Condição simples é apenas com if, condição composta é quando tem o else.
