38c796aa965144c4dbd0320d0978e0637efe5ed2
Marília Queiroz <marisqueiroz1@gmail.com>
2020-12-07 13:21:11 -0300

    Implementações e uso do Switch

--- aulaCondicoes/aulaCondicoesPar2.js
+++ aulaCondicoes/aulaCondicoesPar2.js
@@ -12,11 +12,12 @@ if (idade < 16) {
 
 // -------------------------------------------------------
 
-var hora = 25
+var agora = new Date()
+var hora = agora.getHours() // Pegando as horas atuais, e tem outros gets
 
 if (hora < 12) {
     console.log('Bom dia!')
-} else if (hora >= 12 && hora == 18) {
+} else if (hora <= 18) {
     console.log('Boa tarde')
 } else if (hora > 18 && hora == 24) {
     console.log('Boa noite!')
