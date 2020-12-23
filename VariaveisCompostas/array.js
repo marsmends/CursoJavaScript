e0cefb93def49f9b2c3fa042c40e2c9e1790d03b
Marília Queiroz <marisqueiroz1@gmail.com>
2020-12-23 00:04:47 -0300

    Código simplificado de percorrer um array
    Novo método conforme disponibilizado no ECMA script.

--- VariaveisCompostas/array.js
+++ VariaveisCompostas/array.js
@@ -1,4 +1,8 @@
 let num = [1, 2, 3, 4]
-for (i = 0; i <= num.length; i++) {
+/*for (let i = 0; i <= num.length; i++) {
     console.log(num[i])
+}*/
+
+for(let i in num) {
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
 }
